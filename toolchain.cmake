c6534705c286091201450270d774edef5ccf15e6
Wcg <56055887+wangchenguang123@users.noreply.github.com>
2024-11-30 14:34:07 +0800

    Update toolchain.cmake

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,6 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR riscv64)
 
-set(tools /usr/riscv64-linux-gnu)
-set(CMAKE_C_COMPILER ${tools}/bin/riscv64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/riscv64-linux-gnu-g++)
+set(CMAKE_C_COMPILER riscv64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER riscv64-linux-gnu-g++)
